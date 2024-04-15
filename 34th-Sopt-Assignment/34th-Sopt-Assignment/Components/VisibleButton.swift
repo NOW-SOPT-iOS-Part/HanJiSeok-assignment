aa00e9429e68074e6466e2dee4334ff2f8283a1a
sozohoy <sozohoy@gmail.com>
2024-04-15 18:15:38 +0900

    feat: 삭제, 비밀번호 보기 버튼

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/Components/VisibleButton.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Components/VisibleButton.swift
@@ -5,8 +5,24 @@
 //  Created by 한지석 on 4/15/24.
 //
 
-import Cocoa
+import UIKit
 
 class VisibleButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+    }
+
+    func setUI() {
+        let visibleImage = UIImage(systemName: "eye.circle")
+        self.setImage(visibleImage, for: .normal)
+        self.tintColor = .tvingGray1
+        self.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        self.contentMode = .scaleAspectFit
+    }
 }
