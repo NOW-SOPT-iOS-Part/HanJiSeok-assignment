c10d09f4b17ffd2b4095c4b32f121bc1d74bc7d6
sozohoy <sozohoy@gmail.com>
2024-04-15 23:06:15 +0900

    feat: 텍스트 필드 좌측 패딩, 우측 버튼 기능들 구현

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UITextField+.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UITextField+.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import UIKit
+
+import SnapKit
+
+extension UITextField {
+    func leftPadding() {
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: self.frame.height))
+        self.leftView = leftPaddingView
+        self.leftViewMode = ViewMode.always
+    }
+
+    func idRightView() {
+        let clearButton = ClearButton()
+        clearButton.addTarget(self,
+                              action: #selector(clearButtonTapped),
+                              for: .touchUpInside)
+        let padding = CustomPadding(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: 16,
+                                                  height: 0))
+        padding.widthAnchor.constraint(equalToConstant: 16).isActive = true
+        let buttonWithPadding = UIStackView()
+        buttonWithPadding.axis = .horizontal
+        buttonWithPadding.spacing = 0
+        buttonWithPadding.addArrangedSubview(clearButton)
+        buttonWithPadding.addArrangedSubview(padding)
+        self.rightView = buttonWithPadding
+        self.rightViewMode = .whileEditing
+    }
+
+    func passwordRightView() {
+        let visibleButton = VisibleButton()
+        visibleButton.addTarget(self,
+                                action: #selector(visibleButtonTapped),
+                                for: .touchUpInside)
+        let clearButton = ClearButton()
+        clearButton.addTarget(self,
+                              action: #selector(clearButtonTapped),
+                              for: .touchUpInside)
+        let padding = CustomPadding(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: 0,
+                                                  height: 0))
+        padding.widthAnchor.constraint(equalToConstant: 0).isActive = true
+        let buttonsWithPadding = UIStackView()
+        buttonsWithPadding.axis = .horizontal
+        buttonsWithPadding.spacing = 16
+        buttonsWithPadding.addArrangedSubview(clearButton)
+        buttonsWithPadding.addArrangedSubview(visibleButton)
+        buttonsWithPadding.addArrangedSubview(padding)
+        self.rightView = buttonsWithPadding
+        self.rightViewMode = .whileEditing
+    }
+
+    @objc func clearButtonTapped(_ sender: UIButton) {
+        self.text = ""
+    }
+
+    @objc func visibleButtonTapped(_ sender: UIButton) {
+        self.isSecureTextEntry.toggle()
+        if isSecureTextEntry {
+            let image = UIImage(systemName: "eye.circle")
+            sender.setImage(image, for: .normal)
+        } else {
+            let image = UIImage(systemName: "eye.slash.circle")
+            sender.setImage(image, for: .normal)
+        }
+    }
+}
