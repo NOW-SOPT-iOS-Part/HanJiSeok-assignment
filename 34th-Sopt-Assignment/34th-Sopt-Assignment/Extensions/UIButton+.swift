b76238635d8b7a2eca8d8b716025c8b2882fbf0c
sozohoy <sozohoy@gmail.com>
2024-04-15 18:15:18 +0900

    feat: 버튼 밑줄 메소드

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UIButton+.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UIButton+.swift
@@ -5,4 +5,12 @@
 //  Created by 한지석 on 4/15/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    func addUnderline() {
+        let attributedString = NSMutableAttributedString(string: self.titleLabel?.text ?? "")
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+}
