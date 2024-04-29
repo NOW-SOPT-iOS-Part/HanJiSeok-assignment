0bd74a13aae9369d0eb6d845f32e4c651fdb93ab
sozohoy <sozohoy@gmail.com>
2024-04-29 23:31:40 +0900

    feat: UILabel 사이즈 반환 메소드 구현(#3)

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UILabel+.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Extensions/UILabel+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+extension String {
+    func size(_ font: UIFont) -> CGSize {
+        let size = (self as NSString).size(withAttributes: [.font: font])
+        let buffer = 0.2
+        return CGSize(width: size.width + buffer + 5, height: size.height)
+    }
+}
