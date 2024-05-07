29c4045ac9627ac2369461dc20db307bf8882f62
sozohoy <sozohoy@gmail.com>
2024-05-07 15:50:43 +0900

    feat: MovieListViewController 구현(#5)

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewController/MovieListViewController.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewController/MovieListViewController.swift
@@ -11,11 +11,49 @@ class MovieListViewController: UIViewController {
 
     private let tableView = UITableView(frame: .zero, style: .plain)
 
+    var movieModels: [MovieModel] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         Task {
-            await print(MovieModel.fetchMovie())
+            movieModels = await MovieModel.fetchMovie()
+            self.tableView.reloadData()
         }
+        setUI()
+        register()
+    }
+
+    private func setUI() {
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    private func register() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(
+            MovieCell.self,
+            forCellReuseIdentifier: MovieCell.identifier
+        )
     }
 }
 
+extension MovieListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+}
+
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieModels.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell else { return UITableViewCell() }
+        cell.bindData(movieModels[indexPath.row])
+        return cell
+    }
+}
